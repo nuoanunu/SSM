08fdec056189776afca7e179f8bacbe937be934d
nuoanunu <dwarpro@gmail.com>
2016-09-05 15:39:33 +0700

    tye
    e

---
+++ TEMP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEMP.MvcApplication" Language="C#" %>
---
+++ TEMP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEMP.Startup))]
+namespace TEMP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
