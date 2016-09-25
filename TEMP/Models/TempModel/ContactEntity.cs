a158a8b08789e4a842ffa2d2c42c80bcba0856bf
QuangAndroid <quangFlappy@gmail.com>
2016-09-25 21:35:17 +0700

    Show, Add, Edit Potential Buyer
    With Validation on both Server and Client

---
+++ TEMP/Models/TempModel/ContactEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SSM.Models.TempModel
+{
+    public class ContactEntity
+    {
+        
+        public int id { get; set; }
+        [Required(ErrorMessage ="Please enter customer's last name")]
+        [StringLength(255)]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string Photo { get; set; }
+        [Required]
+        public System.DateTime DateOfBirth { get; set; }
+        [Required]
+        [EmailAddress]
+        public string emails { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        [Url]
+        public string Sites { get; set; }
+        public Nullable<int> CompanyID { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        [Range(10000,999999999)]
+        public string Zip { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
